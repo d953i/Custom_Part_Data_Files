a8e48eeb069c71062a424ce182c65039f43fa2f9
d9 <d9@idv-tech.com>
2020-05-27 09:27:25 -0400

    Make writes/reads sequential

--- Scripts/jtag2axi.tcl
+++ Scripts/jtag2axi.tcl
@@ -1,36 +1,35 @@
 
-proc write32 {addr value} {
+proc write4B {addr value} {
     create_hw_axi_txn -force w0 [get_hw_axis hw_axi_1] -address [format 0x%016lx $addr] -data [format 0x%08lx $value] -type write -len 4
     run_hw_axi -quiet w0
 }
 
-proc write256 {} {
-    set addr 0x400000000
-    set value 0
+proc write256B {} {
+    set base_addr 0x400000000
     for {set i 0} {$i < 64} {incr i} {
-        set addr [expr $addr + 4 * $i]
-        set value 0
+        set addr [expr $base_addr + 4 * $i]
+        set value 55
         set value [expr $value + [expr $i << 0]]
         set value [expr $value + [expr $i << 8]]
         set value [expr $value + [expr $i << 16]]
         set value [expr $value + [expr $i << 24]]
         puts "write to addr: [format 0x%010lx $addr] value: [format 0x%08lx $value]"
-        write32 $addr $value
+        write4B $addr $value
     }
 }
 
-proc read32 {addr} {
+proc read4B {addr} {
     create_hw_axi_txn -force r0 [get_hw_axis hw_axi_1] -address [format 0x%010lx $addr] -type read
     run_hw_axi -quiet r0
-    set d0 [get_property DATA [get_hw_axi_txn r0]]
-    puts "read from addr: [format 0x%09lx $addr] value: $d0"
+    set res [get_property DATA [get_hw_axi_txn r0]]
+    puts "read from addr: [format 0x%010lx $addr] value: [format 0x$res]"
 }
 
-proc read256 {} {
-    set addr 0x400000000
+proc read256B {} {
+    set base_addr 0x400000000
     set value 0
     for {set i 0} {$i < 64} {incr i} {
-        set addr [expr $addr + 4 * $i]
-        read32 $addr
+        set addr [expr $base_addr + 4 * $i]
+        read4B $addr
     }
 }
