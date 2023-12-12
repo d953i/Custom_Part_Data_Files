b8b941d66658f0c44013fb03469ec142d24db5c9
Dmitriy Ivashin <6982205+d953i@users.noreply.github.com>
2023-12-12 11:30:29 -0500

    Added GPL2 notice to all files (in addition to project license file)

--- Scripts/jtag2axi.tcl
+++ Scripts/jtag2axi.tcl
@@ -1,3 +1,22 @@
+###############################################################################
+#
+#  Copyright (c) 2001-2023 divashin@gmail.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+################################################################################
 
 proc write4B {addr value} {
     create_hw_axi_txn -force w0 [get_hw_axis hw_axi_1] -address [format 0x%016lx $addr] -data [format 0x%08lx $value] -type write -len 4
